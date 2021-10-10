27f4de5c16943b861f6702acf2639ae64b998b10
Saharboukari <saharboukari@gmail.com>
2021-10-10 14:29:08 +0100

    six

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-$("registred").on(click,function(){
+$(".registred").on(click,function(){
   var Email=$("#email").val();
   var password=$("#pswd").val();
   var Cpassword=$("#psw-repeat").val();
@@ -16,3 +16,31 @@ $("registred").on(click,function(){
     localStorage.setItem('password', password);
   }
 })
+/////////
+$(function(){
+  $(".confirm-reserve").on("click", function(){
+        var $client=$("#fname").val();
+        localStorage.setItem("getvalue",$client);
+        window.location.href="allreserv.html"
+  })
+  document.getElementById("#reserv-span").innerHTML=localStorage.getItem("getvalue")
+     
+ })
+ //////
+ var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = $(".show");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
