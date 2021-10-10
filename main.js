c4d08f27e31fda382e6dba56c06090329d8cba03
Saharboukari <saharboukari@gmail.com>
2021-10-10 22:25:49 +0100

    seven

--- main.js
+++ main.js
@@ -1,46 +1,40 @@
-$(".registred").on(click,function(){
-  var Email=$("#email").val();
-  var password=$("#pswd").val();
-  var Cpassword=$("#psw-repeat").val();
-  if (Email===""||password===""||Cpassword===""){
+$(".registred").click(function(){
+  var $Email=$("#email").val();
+  var $password=$("#pswd").val();
+  var $Cpassword=$("#psw-repeat").val();
+  if ($Email===""||$password===""||$Cpassword===""){
     alert("please fill all fields..!")
   }
-  else if (password.length<8){
+  else if ($password.length<8){
     alert("password too short")
   }
-  else if(password!==Cpassword){
+  else if($password!==$Cpassword){
     alert("confirm password")
   }
   function saveDta() {
-    localStorage.setItem('email', email);
-    localStorage.setItem('password', password);
+    localStorage.setItem('email', $Email);
+    localStorage.setItem('password', $password);
   }
+
 })
 /////////
-$(function(){
-  $(".confirm-reserve").on("click", function(){
-        var $client=$("#fname").val();
-        localStorage.setItem("getvalue",$client);
-        window.location.href="allreserv.html"
-  })
-  document.getElementById("#reserv-span").innerHTML=localStorage.getItem("getvalue")
+function resevation(){
+        var firstName =document.getElementById("#firstname").value()
+        var lastName= document.getElementById("#lastname").value();
+        var startDate=document.getElementById("#start").value();
+        var backtDate=document.getElementById("#until").value()
+        var carModel=document.getElementById("#country").value();
+        var client=firstName+lastName;
+        var duration=startDate+backtDate;
+        var reservation={
+          Client:client,
+          Duration:duration,
+          Car:carModel,
+        }
+        var res=JSON.stringify(reservation);
+       localStorage.setItem(reservation,res);
+      console.log(localStorage)
+  document.getElementById("#reserv-span").innerHTML=reservation;
      
- })
+ }
  //////
- var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = $(".show");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";  
-}
