2f650fb4e93d2ded23e38c45c5a7d6dbc2ff44c4
Saharboukari <saharboukari@gmail.com>
2021-10-10 13:21:44 +0100

    five

--- main.js
+++ main.js
@@ -1,68 +1,18 @@
-function each (arr,fn)
-{
-	for(var i = 0 ; i<arr.length;i++)
-	{
-	  fn(arr[i],i) 
-	}
-}
-
-function filter(arr , fn)
- {	
- 		if (Array.isArray(arr))
- 			{
- 				var acc = []
- 			}
- 		else
- 			{
- 				var acc = {}
- 			}
-
-
-    each(arr,function(element,i)
-   	     {
-    				 if (fn(element,i))
-    			 	{
-     				 acc.push(element)
-     				}
-
-          })
-    	  return acc 
-        }
-        function f (string)
-        {
-            var format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
-        
-        if(format.test(string)){
-          return true;
-        } else {
-          return false;
-        }
-        
-        }
- function strong ()
- { 
-     var password=$( "#pswd" ).val()
-
-     var format =  /^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/;
-
-     var arr1 = filter(arr, 
-         function(element,i)
-         {
-          return f(element)
-         })
-
-     var arr2 = filter(arr, function(element,i){
-         return element.toUpperCase()===element
-     })
-     
-     var arr3 = filter(arr, function(element,i){
-         return element.toUpperCase()!==element
-     })
-
-    return arr1.length>0 && arr2.length>0 && arr3.length>0 && string.length>8
- }
- $("#registred")
- .first()
- .on("click", function () {
-  strong ();
- });
+$("registred").on(click,function(){
+  var Email=$("#email").val();
+  var password=$("#pswd").val();
+  var Cpassword=$("#psw-repeat").val();
+  if (Email===""||password===""||Cpassword===""){
+    alert("please fill all fields..!")
+  }
+  else if (password.length<8){
+    alert("password too short")
+  }
+  else if(password!==Cpassword){
+    alert("confirm password")
+  }
+  function saveDta() {
+    localStorage.setItem('email', email);
+    localStorage.setItem('password', password);
+  }
+})
